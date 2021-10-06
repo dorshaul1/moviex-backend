7af4f80d26cc1c7f925f01f72a3746cd710d6236
dorshaul1 <dor.asti@gmail.com>
2021-10-06 14:45:14 +0300

    adding movie by actor

--- api/movie/movie.service.js
+++ api/movie/movie.service.js
@@ -151,10 +151,18 @@ async function tmdb_getSimilarMovies(movieId, page = 1) {
 async function tmdb_searchMovie(searchedMovie, page = 1) {
       try {
             const movies = await axios(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${page}&include_adult=false&query=${searchedMovie}`)
+            const actor = await axios(`https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&language=en-US&page=${page}&include_adult=false&query=${searchedMovie}`)
             movies.data.results.map((movie) => {
                   const { movieImg } = _getMovieImages(movie)
                   movie.image = movieImg
             })
+            if (actor.data.results.length !== 0) {
+                  console.log('actor.data.results:', actor.data.results)
+                  actor.data.results[0].image = `https://image.tmdb.org/t/p/original/${actor.data.results[0].profile_path} `
+                  actor.data.results[0].isActor = true
+                  movies.data.results.unshift(actor.data.results[0])
+            }
+
             return movies.data
       } catch (err) {
             console.log('err:', err)
@@ -166,7 +174,22 @@ async function tmdb_getActorDetail(actorId) {
       try {
             const actor = await axios(`https://api.themoviedb.org/3/person/${actorId}?api_key=${API_KEY}&language=en-US`)
             actor.data.image = `https://image.tmdb.org/t/p/original/${actor.data.profile_path}`
+            actor.data.movies = await tmdb_getActorMovies(actorId)
             return actor.data
+      } catch (err) {
+            throw err
+      }
+}
+
+async function tmdb_getActorMovies(actorId) {
+      try {
+            const actor = await axios(`https://api.themoviedb.org/3/person/${actorId}?api_key=${API_KEY}&append_to_response=combined_credits`)
+            actor.data.combined_credits.cast.map((actor) => {
+                  const { movieImg, backDropImg } = _getMovieImages(actor)
+                  actor.image = movieImg
+                  actor.backDrop = backDropImg
+            })
+            return actor.data.combined_credits.cast
       } catch (err) {
             throw err
       }
